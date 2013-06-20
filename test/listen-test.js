79c15897fe0159ddf7ab0635e94e6ee6306f17cd
marceluran <marcelduran@gmail.com>
2013-06-20 14:14:06 -0700

    outputting server url to debug test errors

--- test/listen-test.js
+++ test/listen-test.js
@@ -59,6 +59,7 @@ vows.describe('Dry Run').addBatch({
       var hostname = os.hostname(),
           port = WebPageTest.defaultListenPort;
 
+      console.log(data.url);
       assert.equal(data.protocol, 'http');
       assert.equal(data.hostname, hostname);
       assert.equal(data.port, port);
