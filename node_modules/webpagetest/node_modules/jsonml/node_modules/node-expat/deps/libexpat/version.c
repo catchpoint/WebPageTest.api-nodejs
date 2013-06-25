22c6cce8572da1f80f23df61d9d06a3d24c85390
marceluran <marcelduran@gmail.com>
2013-06-25 11:36:43 -0700

    updated version;cleanup

--- node_modules/webpagetest/node_modules/jsonml/node_modules/node-expat/deps/libexpat/version.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <expat.h>
-
-int main () {
-  XML_Expat_Version v;
-  v = XML_ExpatVersionInfo();
-  printf("Using libexpat v%d.%d.%d\n", v.major, v.minor, v.micro);
-  return 0;
-}
